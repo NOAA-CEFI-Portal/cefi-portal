69cd8704e3d52e2d9f6de56a9c4250a578c77db4
C.-W. HSU <11950284+chiaweh2@users.noreply.github.com>
2024-07-10 11:51:16 -0600

    add gif region for a more dynamic region view

---
+++ overview.js
@@ -0,0 +1,23 @@
+var defaultRegion = 'northwest_atlantic';
+
+$(document).ready(function() {
+    // Listen for changes in the radio buttons
+    $('input[name="regionOptions"]').change(function() {
+        // Get the ID of the selected label
+        var labelId = $(this).next('label').attr('for');
+        console.log('Portal default region :', labelId);
+        // change all regions to the radio options
+        chooseDefaultRegion('reg-mom-cobalt',labelId)
+    });
+});
+
+
+// function for create option for general options
+function chooseDefaultRegion(selectClass,defaultValue) {
+    let elms = document.getElementsByClassName(selectClass);
+    // loop through all region dropdown with the selectClassName
+    for(let i = 0; i < elms.length; i++) {
+        // Set the default option based on the option value
+        elms[i].value = defaultValue
+    }
+};
