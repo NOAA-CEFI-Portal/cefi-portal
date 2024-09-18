1a95af2423c5a0fd5561e91fef4ed08261765b92
C.-W. HSU <11950284+chiaweh2@users.noreply.github.com>
2024-09-18 11:54:34 -0600

    refactor to modern fetch approach

--- lmesPlotlyMapbox.js
+++ lmesPlotlyMapbox.js
@@ -1,7 +1,14 @@
-// read geojson and create plotly plot
-readTextFile("lms_choropleth_mapbox.json", function(text){
-    var datajson = JSON.parse(text);
-    lmesPlotlyMapbox(datajson);
+// Using fetch() to read the JSON file
+fetch('lms_choropleth_mapbox.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); // Parse the JSON
+  })
+  .then(dataJson => {
+    // create the plotly plot
+    lmesPlotlyMapbox(dataJson);
 
     // auto-click the reset button to make the mapbox plot show normal view
     var inputElement = document.querySelector('a[data-title="Reset view"]')
@@ -17,22 +24,10 @@ readTextFile("lms_choropleth_mapbox.json", function(text){
         }
         ,500);
     });
-
-});
-
-// function to read geojson
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-}
-
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
 
 // plot the plotly figure
 function lmesPlotlyMapbox(datajson) {
@@ -44,3 +39,40 @@ function lmesPlotlyMapbox(datajson) {
         )
     }
 }
+
+
+// // read geojson and create plotly plot
+// readTextFile("lms_choropleth_mapbox.json", function(text){
+//     var datajson = JSON.parse(text);
+//     lmesPlotlyMapbox(datajson);
+
+//     // auto-click the reset button to make the mapbox plot show normal view
+//     var inputElement = document.querySelector('a[data-title="Reset view"]')
+//     // first wait for 1 sec for plotly plot in modal to finish and assign the element
+//     setTimeout(()=> {
+//         inputElement = document.querySelector('a[data-title="Reset view"]')
+//     }
+//     ,1000);
+//     // reset button is clicked once the modal open (sufficient time out is necessary)
+//     $('#lmeButton').on("click", function () {
+//         setTimeout(()=> {
+//             inputElement.click()
+//         }
+//         ,500);
+//     });
+
+// });
+
+// // function to read geojson
+// function readTextFile(file, callback) {
+//     var rawFile = new XMLHttpRequest();
+//     rawFile.overrideMimeType("application/json");
+//     rawFile.open("GET", file, true);
+//     rawFile.onreadystatechange = function() {
+//         if (rawFile.readyState === 4 && rawFile.status == "200") {
+//             callback(rawFile.responseText);
+//         }
+//     }
+//     rawFile.send(null);
+// }
+
