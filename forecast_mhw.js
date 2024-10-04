45a313b38abbacc3eeb82725a5db5856da76bfc7
KevinCounts <156142753+KevinCounts@users.noreply.github.com>
2024-10-04 15:41:41 -0600

    Update forecast_mhw.js
    Adding js functionality to new mobile tabs

--- forecast_mhw.js
+++ forecast_mhw.js
@@ -141,6 +141,9 @@ $("#"+dashDropDownID).on("change", function(){
     // change the active navpil
     $("#"+dashNavPillID+" > ul.nav-pills > li.nav-item").removeClass("active"); 
     $("#"+selectedValue.slice(0, -3)+'Pill').addClass("active");
+    // change the active navtab
+    $("#"+dashNavPillID+" > ul.nav-tabs > li.nav-item").removeClass("active"); 
+    $("#"+selectedValue.slice(0, -3)+'Tab').addClass("active");
     // change the active navpil content
     $("#"+dashContentID+" div.tab-pane").removeClass("active"); 
     $("#"+selectedValue.slice(0, -3)).addClass("active");
@@ -148,6 +151,14 @@ $("#"+dashDropDownID).on("change", function(){
 
 // event listener for navpil being clicked
 $("#"+dashNavPillID+" > ul.nav-pills > li.nav-item > .nav-link").on('click',function(){
+    let hrefID = $(this).attr('href')
+    let hrefIDText = hrefID.slice(1)
+    // reuse changeDashSelect (historical.js) 
+    window.changeDashSelect(dashDropDownID,hrefIDText+'Val')
+});
+
+// event listener for navtab being clicked
+$("#"+dashNavPillID+" > ul.nav-tabs > li.nav-item > .nav-link").on('click',function(){
     let hrefID = $(this).attr('href')
     let hrefIDText = hrefID.slice(1)
     // reuse changeDashSelect (historical.js) 
